34688502c8d45d396367c9f44289a89318136ecc
sasha <gusenichka2004@gmail.com>
2025-02-17 20:30:26 +0700

    1

--- main.c
+++ main.c
@@ -15,11 +15,11 @@ double Bi(double time_step, double space_step)
 }
 double Ci(double time_step, double space_step)
 {
-    return 3;
+    return 3.0;
 }
 double Fi(int n, int j, double **u)
 {
-    return 4 * u[n][j] - u[n - 1][j];
+    return 4.0 * u[n][j] - u[n - 1][j];
 }
 void x_filling(double *x_points, int n, double start, double finish)
 {
@@ -34,9 +34,10 @@ void x_filling(double *x_points, int n, double start, double finish)
 }
 void file_write(FILE *file_graph, double **u, int time_parts, int space_parts, double *time_points, double *space_points)
 {
+    FILE *f1 = fopen("dataut.txt", "r");
     for (int n = 0; n < time_parts; ++n)
     {
-        for (int j = 0; j < time_parts; ++j)
+        for (int j = 0; j < space_parts; ++j)
             fprintf(file_graph, "%.15lf %.15lf %.15lf\n", time_points[n], space_points[j], u[n][j]);
     }
 }
@@ -62,7 +63,7 @@ void progonka(double *time_points, double *space_points, double **u, int time_pa
     }
     for (int i = 1; i < space_parts; ++i)
     {
-        v_points[i] = -((Ai(time_step, space_step) * v_points[i - 1] - Fi(time_step, space_step, u)) / (Ci(time_step, space_step) + Ai(time_step, space_step) * k_points[i - 1]));
+        v_points[i] = -((Ai(time_step, space_step) * v_points[i - 1] - Fi(n-1, i, u)) / (Ci(time_step, space_step) + Ai(time_step, space_step) * k_points[i - 1]));
     }
     u[n][space_parts - 1] = (v_points[space_parts - 1] + k_points[space_parts - 1] * v_points[space_parts - 2]) / (1 - k_points[space_parts - 1] * k_points[space_parts - 2]);
     for (int i = space_parts - 2; i >= 0; --i)
@@ -80,6 +81,7 @@ int main()
     double *space_points = malloc(sizeof(double) * space_parts);
     x_filling(time_points, time_parts, time_start, time_end);
     x_filling(space_points, space_parts, space_start, space_end);
+    FILE *f = fopen("data.txt", "w");
     for (int i = 0; i < time_parts; ++i)
     {
         u[i] = malloc(sizeof(double) * space_parts);
@@ -92,6 +94,16 @@ int main()
     {
         u[0][i] = 0;
     }
+    for(int i = 1; i < space_parts; ++i)
+    {
+        u[1][i] = u[0][i] - (time_step/space_step)*(u[0][i] - u[0][i-1]);
+    }
+    for(int n = 2; n<time_parts;++n)
+    {
+        progonka(time_points, space_points, u, time_parts, space_parts, time_step, space_step, n);
+    }
+    file_write(f,u,time_parts,space_parts,time_points, space_points);
+    
 
     return 0;
 }
